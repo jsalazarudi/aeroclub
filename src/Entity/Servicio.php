d2678c34f0981f9573746e5168dad28cc43d0d74
Jairo Salazar <jsalazar8@udi.edu.co>
2023-04-16 16:47:13 -0500

    RF: Validacion codigo unico Servicio

--- src/Entity/Servicio.php
+++ src/Entity/Servicio.php
@@ -7,9 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ServicioRepository::class)]
+#[UniqueEntity('codigo')]
 class Servicio
 {
     #[ORM\Id]
